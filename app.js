39db5d59ba24cdd1079c2fd3731308308957362f
SandwichDream <karnll.adnitorll@gmail.com>
2023-05-25 17:53:32 +0300

    hw-16-23032023-KyrMov

---
+++ app.js
@@ -0,0 +1,41 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            order: 0,
+            products: [
+                {
+                    name: 'Milk',
+                    quantity: 10,
+                    price: '40uah'
+                },
+                {
+                    name: 'Bread',
+                    quantity: 5,
+                    price: '24uah'
+                },
+                {
+                    name: 'Candy',
+                    quantity: 50,
+                    price: '20uah'
+                },
+                {
+                    name: 'Cola',
+                    quantity: 3,
+                    price: '18uah'
+                }
+            ]
+        }
+    },
+    methods: {
+        addToCart(product) {
+            if (product.quantity > 0) {
+              product.quantity--;
+              this.order++;
+            }
+        }
+    },
+    computed:{
+
+    }
+});
+app.mount("#app");
